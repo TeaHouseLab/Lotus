541eaadc3d46a36a6e3e62f31e68b96d24d74004
tsingkwai <tsingkwai@protonmail.com>
2023-01-13 16:21:57 +0800

    Modify to meet my needs

---
+++ tar_with_dependence.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+set -e
+
+ROOT_DIR="$(git rev-parse --show-toplevel)"
+APP_BUILD_DIR="$ROOT_DIR/build"
+APP_DIR="$APP_BUILD_DIR/package"
+# FISH_NCURSES_ROOT must be provided externally.
+
+env \
+  CXXFLAGS='-static-libgcc -static-libstdc++ -DTPUTS_USES_INT_ARG' \
+  LDFLAGS='-static-libgcc -static-libstdc++' \
+  cmake -S "$ROOT_DIR" -B "$APP_BUILD_DIR" \
+  -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+  -DCMAKE_POLICY_DEFAULT_CMP0074=NEW \
+  -DCMAKE_FIND_ROOT_PATH_MODE_LIBRARY=ONLY \
+  -DCMAKE_INSTALL_PREFIX=/usr \
+  -DBUILD_DOCS=ON \
+  -DWITH_GETTEXT=OFF \
+  -DFISH_USE_SYSTEM_PCRE2=OFF \
+  -DFISH_ALLOW_UNSUPPORTED_STATIC_LINKING=ON \
+  -DCurses_ROOT="$FISH_NCURSES_ROOT" \
+  -DCURSES_NEED_NCURSES=ON
+
+make -C "$APP_BUILD_DIR" -j$(nproc)
+make -C "$APP_BUILD_DIR" install DESTDIR="$APP_DIR"
+
+rm -f "$APP_DIR/usr/bin/fish_indent"
+rm -f "$APP_DIR/usr/bin/fish_key_reader"
+rm -rf "$APP_DIR/usr/share/doc/fish"
+
+mkdir -p "$APP_DIR/usr/share/metainfo/"
+cp "$ROOT_DIR/fish.appdata.xml" "$APP_DIR/usr/share/metainfo/"
+
+cp -r "$FISH_NCURSES_ROOT/share/terminfo" "$APP_DIR/usr/share/"
+
+cat << 'EOF' > "$APP_DIR/lotus"
+#!/bin/bash
+unset ARGV0
+export TERMINFO_DIRS="$package/usr/share/terminfo:$TERMINFO_DIRS"
+exec "$(dirname "$(readlink  -f "${0}")")/usr/bin/fish" ${@+"$@"}
+EOF
+chmod 755 "$APP_DIR/lotus"
+tar zcvf package.tar.gz "$APP_DIR"
