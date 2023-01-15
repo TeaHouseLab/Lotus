72b081460911824ba91274bd2b3dbc08f24fdb85
tsingkwai <tsingkwai@protonmail.com>
2023-01-15 18:53:17 +0800

    fix script

--- install.sh
+++ install.sh
@@ -2,4 +2,4 @@
 wget https://github.com/TeaHouseLab/Lotus/releases/download/latest/lotus.tar.gz
 tar xf lotus.tar.gz
 mv lotus /opt
-ln -s /usr/bin/lotus /opt/lotus/lotus
+ln -s /opt/lotus/lotus /usr/bin/lotus
